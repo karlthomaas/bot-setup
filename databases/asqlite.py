0aabb3e6f7c735bcf19504ed1f46f45e3064e4a6
Karl-Thomas <karl-thomas@zink.ee>
2022-04-12 17:30:43 +0000

    Few updates

---
+++ databases/asqlite.py
@@ -0,0 +1,11 @@
+import aiosqlite
+
+class Sqldb:
+    def __init__(self):
+        self.db = None
+        self.cursor = None
+
+    async def create_connection(self):
+        print('Connection established')
+        self.db = await aiosqlite.connect('./databases/database.db')
+        self.cursor = await self.db.cursor()
